88e19ec9df5065670d0b15de4d78a97bcabe8ffb
show <honokani.kaoriga@gmail.com>
2018-07-13 08:00:20 +0000

    UPDATE input code

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -6,10 +6,25 @@ import Control.Applicative
 
 someFunc :: IO ()
 someFunc = do
-    print =<< inputNumList
+    print =<< getN_and_Nlines
 
-inputNumList :: (Num a, Read a) => IO [a]
-inputNumList = do
+-- # input -> output
+-- "2"         [[12, 34],[567]]
+-- "12   34"
+-- " 567   "
+getN_and_Nlines :: (Num a, Read a, Enum a, Show a) => IO [[a]]
+getN_and_Nlines = do
+    n <- read.head.words <$> getLine
+    l <- mapM (\_ -> getNumberList) [1..n]
+    return l
+
+
+-- # input
+-- "10 20 30"
+-- # outout
+-- [10, 20, 30]
+getNumberList :: (Num a, Read a) => IO [a]
+getNumberList = do
     ws <- map read.words <$> getLine
     return ws
 

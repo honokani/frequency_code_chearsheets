f199f291ce1f1f2ab4c7b68b07ea617d1460c6bd
show <honokani.kaoriga@gmail.com>
2018-08-01 01:34:45 +0000

    UPDATE more Strict

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -7,7 +7,7 @@ import Control.Monad
 
 someFunc :: IO ()
 someFunc = do
-    print =<< getData_and_Nnums
+    print =<< getIN_and_InfoWithNums
 
 ------------------------------------------------------------------------
 -- # input     -> outout
@@ -17,17 +17,28 @@ getWords = words <$> getLine
 
 -- # input    -> outout
 -- "10 20 30"    [10, 20, 30]
-getNumbers :: (Num a, Read a) => IO [a]
+getNumbers :: (Num a, Read a, Enum a) => IO [a]
 getNumbers = map read.words <$> getLine
 
 ------------------------------------------------------------------------
 -- # input     -> output
+-- "2"            ["aaa", "bb"]
+-- "aaa bb cc"
+getN_and_Strs :: IO [String]
+getN_and_Strs = do
+    n <- read.head.words <$> getLine
+    take n <$> getWords
+    where
+        getWords :: IO [String]
+        getWords = words <$> getLine
+
+-- # input     -> output
 -- "2"            [12, 34]
 -- "12 34 567"
-getN_and_Nums :: (Num a, Read a, Enum a, Show a) => IO [a]
+getN_and_Nums :: (Num a, Read a, Enum a) => IO [a]
 getN_and_Nums = do
     n <- read.head.words <$> getLine
-    take n.<$> getNumbers
+    take n <$> getNumbers
     where
         getNumbers :: (Num a, Read a) => IO [a]
         getNumbers = map read.words <$> getLine
@@ -49,7 +60,7 @@ getL_and_Strs = do
 -- "2"        [[12, 34],[567]]
 -- "12 34"
 -- "567"
-getL_and_Nums :: (Num a, Read a, Enum a, Show a) => IO [[a]]
+getL_and_Nums :: (Num a, Read a, Enum a) => IO [[a]]
 getL_and_Nums = do
     l <- read.head.words <$> getLine
     replicateM l getNumbers
@@ -64,15 +75,13 @@ getL_and_Nums = do
 -- "opqr 11 lm"
 getLN_and_Strs :: IO [[String]]
 getLN_and_Strs = do
-    (l, n) <- mapT2 read.head.listToT2s.words <$> getLine
+    (l, n) <- mapT2 read.listToT2.take 2.words <$> getLine
     replicateM l $ take n <$> getWords
     where
+        listToT2 :: [a] -> (a,a)
+        listToT2 (x:y:_) = (x,y)
         mapT2 :: (a -> b) -> (a,a) -> (b,b)
         mapT2 f (a1,a2) = (f a1, f a2)
-        listToT2s :: [a] -> [(a,a)]
-        listToT2s [] = []
-        listToT2s (x:[]) = []
-        listToT2s (x:y:zs) = (x,y) : listToT2s zs
         getWords :: IO [String]
         getWords = words <$> getLine
 
@@ -80,54 +89,64 @@ getLN_and_Strs = do
 -- "2 2"            [[12, 34],[567,89]]
 -- "12 34 555"
 -- "567 89 0000"
-getLN_and_Nums :: (Num a, Read a, Enum a, Show a) => IO [[a]]
+getLN_and_Nums :: (Num a, Read a, Enum a) => IO [[a]]
 getLN_and_Nums = do
-    (l, n) <- mapT2 read.head.listToT2s.words <$> getLine
+    (l, n) <- mapT2 read.listToT2.take 2.words <$> getLine
     replicateM l $ take n <$> getNumbers
     where
+        listToT2 :: [a] -> (a,a)
+        listToT2 (x:y:_) = (x,y)
         mapT2 :: (a -> b) -> (a,a) -> (b,b)
         mapT2 f (a1,a2) = (f a1, f a2)
-        listToT2s :: [a] -> [(a,a)]
-        listToT2s [] = []
-        listToT2s (x:[]) = []
-        listToT2s (x:y:zs) = (x,y) : listToT2s zs
         getNumbers :: (Num a, Read a) => IO [a]
         getNumbers = map read.words <$> getLine
 
 -- # input -> output
--- "2 1"      ["a","o"]
+-- "2 1"      ['a','o'] (=="ao")
 -- "abc"
 -- "opqr"
 getLN_and_Chrs :: IO [String]
 getLN_and_Chrs = do
-    (l, n) <- mapT2 read.head.listToT2s.words <$> getLine
+    (l, n) <- mapT2 read.listToT2.take 2.words <$> getLine
     replicateM l $ take n.head <$> getWords
     where
+        listToT2 :: [a] -> (a,a)
+        listToT2 (x:y:_) = (x,y)
         mapT2 :: (a -> b) -> (a,a) -> (b,b)
         mapT2 f (a1,a2) = (f a1, f a2)
-        listToT2s :: [a] -> [(a,a)]
-        listToT2s [] = []
-        listToT2s (x:[]) = []
-        listToT2s (x:y:zs) = (x,y) : listToT2s zs
         getWords :: IO [String]
         getWords = words <$> getLine
 
 ------------------------------------------------------------------------
 -- # input     -> output
--- "100 2"        (100, [12, 34])
--- "12 34 555"
-getData_and_Nnums :: (Num a, Read a, Enum a, Show a) => IO (Int, [a])
-getData_and_Nnums = do
-    (d, n) <- mapT2 read.head.listToT2s.words <$> getLine
-    xs <- take n <$> getNumbers
-    return (d, xs)
+-- "100 2"        ("100", ["ab", "cd"])
+-- "ab cd eee"
+getIN_and_InfoWithStrs:: IO (String, [String])
+getIN_and_InfoWithStrs = do
+    (i, n) <- mapT2snd read.listToT2.take 2.words <$> getLine
+    xs <- take n <$> getWords
+    return (i, xs)
     where
-        mapT2 :: (a -> b) -> (a,a) -> (b,b)
-        mapT2 f (a1,a2) = (f a1, f a2)
-        listToT2s :: [a] -> [(a,a)]
-        listToT2s [] = []
-        listToT2s (x:[]) = []
-        listToT2s (x:y:zs) = (x,y) : listToT2s zs
+        listToT2 :: [a] -> (a,a)
+        listToT2 (x:y:_) = (x,y)
+        mapT2snd :: (a -> b) -> (a,a) -> (a,b)
+        mapT2snd f (a1,a2) = (a1, f a2)
+        getWords :: IO [String]
+        getWords = words <$> getLine
+
+-- # input     -> output
+-- "100 2"        ("100", [12, 34])
+-- "12 34 555"
+getIN_and_InfoWithNums:: (Num a, Read a, Enum a) => IO (String, [a])
+getIN_and_InfoWithNums = do
+    (i, n) <- mapT2snd read.listToT2.take 2.words <$> getLine
+    xs <- take n <$> getNumbers
+    return (i, xs)
+    where
+        listToT2 :: [a] -> (a,a)
+        listToT2 (x:y:_) = (x,y)
+        mapT2snd :: (a -> b) -> (a,a) -> (a,b)
+        mapT2snd f (a1,a2) = (a1, f a2)
         getNumbers :: (Num a, Read a) => IO [a]
         getNumbers = map read.words <$> getLine
 

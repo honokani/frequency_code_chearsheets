b7c47384830042de4365fdd8a038d69ac0e8e180
Ubuntu <honokani.kaoriga@gmail.com>
2018-09-19 18:17:58 +0900

    ADD haskell sandbox

---
+++ haskell_sandbox/plotdots/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for plotdots
+
+## Unreleased changes
---
+++ haskell_sandbox/plotdots/src/Lib.hs
@@ -0,0 +1,67 @@
+module Lib
+    ( runPlotting
+    ) where
+
+import qualified Control.Concurrent      as CC (forkIO, threadDelay)
+import           Control.Concurrent.MVar
+import           System.Console.ANSI
+import           Control.Monad
+
+data Event = Quit
+           | KB String
+           deriving (Show, Eq)
+
+runPlotting :: IO ()
+runPlotting = do
+
+    status <- newEmptyMVar :: IO (MVar Event)
+    CC.forkIO $ forM_ (cycle [0]) $ \_ -> do
+        inputKeyboard status
+
+    printKB $ "start"
+    untilLiving $ recieveEvent status
+    printKB $ "end"
+
+
+seconds :: Int -> Int
+seconds n = n*1000000
+
+
+untilLiving :: IO Event -> IO ()
+untilLiving act = do
+    e <- act
+    if checkLiving e then do
+        activateEvent e
+        untilLiving act
+    else
+        printKB $ "quited"
+
+checkLiving Quit = False
+checkLiving _    = True
+
+activateEvent :: Event -> IO ()
+activateEvent e = case e of
+    Quit -> return ()
+    KB s -> printKB s
+
+recieveEvent :: (MVar Event) -> IO Event
+recieveEvent mv = do
+    CC.threadDelay 10
+    takeMVar mv
+
+inputKeyboard :: (MVar Event) -> IO ()
+inputKeyboard mv = do
+    CC.threadDelay 10
+    l <- getLine
+    if l=="quit" then
+        putMVar mv $ Quit
+    else
+        putMVar mv $ KB l
+
+printKB s = do
+    clearScreen
+    setCursorPosition　1 1
+    putStrLn "command - quit: quit, "
+    setCursorPosition　2 1
+    putStrLn s
+
